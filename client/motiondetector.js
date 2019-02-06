85c07d35eb8633d5215e7b10fb9a50a2304f2a4b
Alex Witkowski <alex@sibipro.com>
2019-02-06 15:15:47 -0700

    a bunch

---
+++ client/motiondetector.js
@@ -0,0 +1,19 @@
+var Gpio = require('onoff').Gpio,
+  buzzer = new Gpio(18, 'out'),
+  pir = new Gpio(17, 'in', 'both');
+ 
+pir.watch(function(err, value) {
+  if (err) exit();
+  buzzer.writeSync(value);
+  console.log('motion detected');
+  if(value == 1) {
+      // websocket send function goes here
+  }
+});
+ 
+ 
+function exit() {
+  buzzer.unexport();
+  pir.unexport();
+  process.exit();
+}
